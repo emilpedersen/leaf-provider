595db0e2928377790f595b3a4ba0162c42616405
Emil Pedersen <offonline@me.com>
2019-04-23 07:45:16 +0200

    test

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "LeafProvider",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.0.0")),
-        .Package(url: "https://github.com/vapor/leaf.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/leaf.git", .upToNextMajor(from: "2.0.0")),
     ]
 )
