4df420f5c4e56419766f3ec34f7c9e54077b8a99
Emil Pedersen <offonline@me.com>
2019-04-23 07:43:24 +0200

    test

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "LeafProvider",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/leaf.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.0.0")),
+        .Package(url: "https://github.com/vapor/leaf.git", .upToNextMajor(from: "2.0.0")),
     ]
 )
