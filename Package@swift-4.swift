d87786b4fb8180cb83505bbfd661d99c1bbbcae1
Emil Pedersen <offonline@me.com>
2019-12-13 08:01:48 +0100

    test

---
+++ Package@swift-4.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "LeafProvider",
+    products: [
+    	.library(name: "LeafProvider", targets: ["LeafProvider"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/leaf.git", .upToNextMajor(from: "2.0.0")),
+    ],
+    targets: [
+    	.target(name: "LeafProvider", dependencies: ["Leaf", "Vapor"]),
+    	.testTarget(name: "LeafProviderTests", dependencies: ["LeafProvider", "Testing"])
+    ]
+)
