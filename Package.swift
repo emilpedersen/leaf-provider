067fc4ed34d564d8e7d36d2c8128f43209dc24ec
Emil Pedersen <offonline@me.com>
2019-12-13 09:33:15 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "LeafProvider",
+    products: [
+    	.library(name: "LeafProvider", targets: ["LeafProvider"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/emilpedersen/leaf.git", .upToNextMajor(from: "2.0.0")),
+    ],
+    targets: [
+    	.target(name: "LeafProvider", dependencies: ["Leaf", "Vapor"]),
+    	.testTarget(name: "LeafProviderTests", dependencies: ["LeafProvider"])
     ]
 )
